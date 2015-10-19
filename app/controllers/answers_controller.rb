5ec1d46ca1d97b8d77f363e9353d77e963c11da8
toma apostolov & christian lei <student@epicodus.com>
2015-10-19 16:43:29 -0700

    working on upvotes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,50 @@
 class AnswersController < ApplicationController
+
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new
+  end
+
+  def create
+    @user = current_user
+    @question = @user.questions.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+    if @answer.save &&  @answer.update(user_id: @user.id)
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = current_user
+    @question = @user.questions.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    binding.pry
+    if params[:upvote] == true
+      upvote = @answer.upvote
+      if upvote != nil
+        upvote = upvote + 1
+        @answer.update(upvote: upvote)
+        # redirect_to question_path(@question)
+      else
+        @answer.update(upvote: 1)
+        # redirect_to question_path(@question)
+    end
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+
+    redirect_to question_path(@question)
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:content)
+  end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,13 +5,24 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
 
+  def show
+    @question = Question.find(params[:id])
   end
 
   def create
     @user = current_user
+    if @user
     @question = @user.questions.new(question_params)
-    if @question.save
+    end
+    if @user == nil
+      flash[:alert] = "Please log in."
+      @question = Question.new
+      render :new
+
+    elsif @question.save
       redirect_to questions_path
     else
       redirect_to questions_path
@@ -21,6 +32,9 @@ class QuestionsController < ApplicationController
   def destroy
     @user = current_user
     @question = @user.questions.find(params[:id])
+    @question.answers.each do |answer|
+      answer.destroy
+    end
     if @question.destroy
       flash[:notice] = "Question deleted"
       redirect_to questions_path
