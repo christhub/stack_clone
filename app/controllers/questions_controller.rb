b67fe27cca607365847c5df5362ce241bc54a587
toma apostolov & christian lei <student@epicodus.com>
2015-10-19 15:08:47 -0700

    delete questions only if logged in

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,5 +9,29 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @question = @user.questions.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      redirect_to questions_path
+    end
   end
+
+  def destroy
+    @user = current_user
+    @question = @user.questions.find(params[:id])
+    if @question.destroy
+      flash[:notice] = "Question deleted"
+      redirect_to questions_path
+    else
+      redirect_to questions_path
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:question, :content)
+  end
+
 end
