0212e1c76fbf8be367d06c4c26bbb8027f059705
toma apostolov & christian lei <student@epicodus.com>
2015-10-19 14:13:25 -0700

    authentication works. log-in and log-out.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      flash[:notice] = "You've been logged in."
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:alert] = "There was a problem loggin you in."
+      redirect_to log_in_path
+    end
+  end
+
+    def destroy
+      session[:user_id] = nil
+      flash[:notice] = "You've been logged out successfully."
+      redirect_to root_path
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
     resources :answers, :except => [:show, :index]
   end
   resources :users
+
+  # resources :sessions, :only => [:new, :create, :destroy]
+
+  get "/log-in" => "sessions#new"
+  post "/log-in" => "sessions#create"
+  get "/log-out" => "sessions#destroy", as: :log_out
 end
